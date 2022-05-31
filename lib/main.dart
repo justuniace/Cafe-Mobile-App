7c431802ebbbe0be7f3b6eadae2bd42cd3f2827e
Jazen <jazencode@gmail.com>
2022-05-31 15:53:31 +0800

    Added assest

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:coffee_app/provider/animationLoading.dart';
 import 'package:coffee_app/screens/cart/cart.dart';
 import 'package:coffee_app/screens/favorite/favorite.dart';
 import 'package:coffee_app/screens/home/home.dart';
+import 'package:coffee_app/screens/login/login.dart';
 import 'package:coffee_app/screens/profile/profile.dart';
+import 'package:coffee_app/screens/register/register.dart';
 import 'package:coffee_app/screens/welcome/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -28,6 +30,8 @@ void main() => runApp(
       initialRoute: '/welcome',
       routes: {
         '/welcome': (context) => const Welcome(),
+        '/login': (context) => const Login(),
+        '/register': (context) => const Register(),
         '/': (context) => const Home(),
         '/favorite': (context) => const Favorite(),
         '/cart': (context) => const Cart(),
