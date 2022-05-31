9a690212534cb5b04d682aa779c3b7c74f45c510
Jazen <jazencode@gmail.com>
2022-05-31 16:42:14 +0800

    okii na

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'package:coffee_app/components/colors.dart';
 import 'package:coffee_app/provider/animationLoading.dart';
 import 'package:coffee_app/screens/cart/cart.dart';
@@ -10,7 +11,20 @@ import 'package:coffee_app/screens/welcome/welcome.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(
+  ChangeNotifierProvider(
+    create: (context) => AnimationLoading(),
+    child: MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Helvetica',
+        
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            fontSize: 18.0,
+            color: AppColor.darkLightColor
+          ),
+        )
+      ),
       title: 'CupFé',
       debugShowCheckedModeBanner: false,
       initialRoute: '/welcome',
@@ -23,4 +37,6 @@ void main() => runApp(MaterialApp(
         '/cart': (context) => const Cart(),
         '/profile': (context) => const Profile(),
       },
-    ));
+    ),
+  )
+);
